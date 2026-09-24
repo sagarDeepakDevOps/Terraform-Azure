762b0eb1eb116b948398d9bb7c5488cbf0aab7d2
SagarDeepakDevOps <sagardeepak2002@gmail.com>
2026-09-24 17:13:44 +0530

    Remove commented instructions for backend initialization in Terraform configuration

--- full-lab/backend.tf
+++ full-lab/backend.tf
@@ -1,6 +1,5 @@
-# After applying backend-prereq, uncomment and run: terraform init -backend-config=backend.hcl -migrate-state
 terraform {
   backend "azurerm" {
     key = "full-lab.tfstate"
   }
-# }
+}
